d0c70591b7417ed0466a8d182af51f99cc693826
aakash819 <aakash.goel462@gmail.com>
2019-08-12 18:40:36 +0530

    add form key in ts file and add control name in html file

--- src/app/modules/branding/modules/banner/components/add-new-banner/add-new-banner.component.ts
+++ src/app/modules/branding/modules/banner/components/add-new-banner/add-new-banner.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BannerTypeEnum } from 'src/app/modules/branding/enums/banner-type.enum';
 
 @Component({
   selector: 'app-add-new-banner',
@@ -13,15 +14,29 @@ export class AddNewBannerComponent implements OnInit {
 
   public createBannerForm() {
     return this.fb.group({
-      
-    })
+      bannerId: [null],
+      campaignId: [null],
+      bannerName: ['', Validators.required],
+      bannerType: [BannerTypeEnum.Standard],
+      bannerImage: [null, Validators.required],
+      width: ['', Validators.required],
+      height: ['', Validators.required],
+      trackingPixel: '',
+      comments: '',
+      keyword: '',
+      destinationUrl: ['', Validators.required],
+      weight: null,
 
+    })
   }
 
 
+  public onSubmit() {
 
-
+    console.log(this.bannerForm.value);
+  }
   ngOnInit() {
+    this.bannerForm = this.createBannerForm();
   }
 
 }
