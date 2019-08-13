3c5b30ce729a9b32a738fdfd26e0432084742235
aakash819 <aakash.goel462@gmail.com>
2019-08-13 14:03:13 +0530

    add all modules or add new function for  get all banner

--- src/app/modules/branding/modules/banner/components/all-banners/all-banners.component.ts
+++ src/app/modules/branding/modules/banner/components/all-banners/all-banners.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort, MatSnackBar } from '@angular/material';
+import { BannerModel } from 'src/app/modules/branding/model/banner.model';
+import { BannerService } from 'src/app/modules/branding/services/banner/banner.service';
 
 @Component({
   selector: 'app-all-banners',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-banners.component.scss']
 })
 export class AllBannersComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'date', 'details', 'action'];
+  dataSource: MatTableDataSource<BannerModel>;
+  public errMsg: string = ''
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private bannerService: BannerService, private snackbar: MatSnackBar) { }
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+
+
+
+  public getBanner() {
+    this.bannerService.getBanner()
+      .then(resp => {
+        console.log(resp)
+        this.dataSource = new MatTableDataSource<BannerModel>(resp)
+      })
+      .catch(err => {
+        this.errMsg = err;
+      })
+  }
+
+
 
   ngOnInit() {
+    this.getBanner()
   }
 
 }
