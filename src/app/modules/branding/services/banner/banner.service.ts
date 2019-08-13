56f9286d145a87bb55d7c5a6158f666bb279aca5
aakash819 <aakash.goel462@gmail.com>
2019-08-13 16:33:26 +0530

    add  new function for edit banner

--- src/app/modules/branding/services/banner/banner.service.ts
+++ src/app/modules/branding/services/banner/banner.service.ts
@@ -4,6 +4,7 @@ import { BannerTypeEnum } from '../../enums/banner-type.enum';
 import { HttpService } from '../http/http.service';
 import { BrandingModule } from '../../branding.module';
 
+
 @Injectable({
   providedIn: BrandingModule
 })
@@ -57,6 +58,13 @@ export class BannerService {
     });
   }
 
+
+  /**
+   * add new banner 
+   * @param data form data
+   */
+
+
   public addBanner(data): Promise<BannerModel> {
     return new Promise((resolve, reject) => {
       this.httpServce.post(this.baseUrl, data)
@@ -69,6 +77,30 @@ export class BannerService {
         })
     })
   }
+
+
+
+  /***
+   * edit banner by banner id  
+   * @param bannerId id of banner 
+   * @param data data to send for edit
+   */
+  public editBanner(bannerId: number, data): Promise<BannerModel> {
+    return new Promise((resolve, reject) => {
+      this.httpServce.put(this.baseUrl + bannerId + '/', data)
+        .then(resp => {
+          let banner = this.parseBanner([resp.data]);
+          resolve(banner[0]);
+        })
+        .catch(err => {
+          reject(err)
+        })
+    })
+  }
+
+
+
+
 
   /**
    * delete banner with banner id 
