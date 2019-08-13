a4819f0d3b69ec061af6f38a8764639775d4bd57
aakash819 <aakash.goel462@gmail.com>
2019-08-13 10:51:19 +0530

    add new function for get all banner and get banner with id

--- src/app/modules/branding/services/banner/banner.service.ts
+++ src/app/modules/branding/services/banner/banner.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { BannerModel } from '../../model/banner.model';
 import { BannerTypeEnum } from '../../enums/banner-type.enum';
+import { HttpService } from '../http/http.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BannerService {
 
-  constructor() { }
+  public baseUrl = 'banners/'
+
+  constructor(private httpServce: HttpService) { }
 
 
   private bannerList: BannerModel[] = Array.from({ length: 20 }, (_, i) => new BannerModel(
@@ -20,9 +23,46 @@ export class BannerService {
     }
   ));
 
-  public getBanner() {
-    return new Promise((resolve, reject) => {
 
+  /**
+   * get all banner and get banner with id 
+   * @param bannerId id of banner
+   */
+
+  public getBanner(): Promise<BannerModel[]>;
+  public getBanner(bannerId: number): Promise<BannerModel>;
+  public getBanner(bannerId?: number): Promise<BannerModel | BannerModel[]> {
+    return new Promise((resolve, reject) => {
+      if (bannerId) {
+        this.httpServce.get(this.baseUrl + bannerId + '/')
+          .then(resp => {
+            let banner = this.parseBanner([resp.data])
+            resolve(banner[0])
+          })
+          .catch(err => {
+            reject(err);
+          })
+      }
+      else {
+        this.httpServce.get(this.baseUrl)
+          .then(resp => {
+            let banner = this.parseBanner(resp.data)
+          })
+      }
     });
   }
+
+
+
+
+
+
+
+ 
+
 }
+
+
+
+
+
