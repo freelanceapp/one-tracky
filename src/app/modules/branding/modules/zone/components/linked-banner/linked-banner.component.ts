610657b2958da0d42d23025d5dfd9367285862ea
aakash819 <aakash.goel462@gmail.com>
2019-08-26 14:21:01 +0530

    add code for get zone and website id

--- src/app/modules/branding/modules/zone/components/linked-banner/linked-banner.component.ts
+++ src/app/modules/branding/modules/zone/components/linked-banner/linked-banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-linked-banner',
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./linked-banner.component.scss']
 })
 export class LinkedBannerComponent implements OnInit {
+  public websiteId: number = null;
+  public zoneId: number = null;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) {
+    if (this.activatedRoute.parent.snapshot.paramMap.get('websiteId')) {
+      this.websiteId = parseInt(this.activatedRoute.parent.snapshot.paramMap.get('websiteId'), 10);
+    }
+    if (this.activatedRoute.parent.snapshot.paramMap.get('zoneId')) {
+      this.zoneId = parseInt(this.activatedRoute.parent.snapshot.paramMap.get('zoneId'), 10);
+    }
+  }
 
   ngOnInit() {
   }
