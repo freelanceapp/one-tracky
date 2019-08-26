371f9245e9df90b1d7cb5ebd0878ddc00adc56ee
aakash819 <aakash.goel462@gmail.com>
2019-08-26 14:18:07 +0530

    add new function for get websiteId and zoneId

--- src/app/modules/branding/modules/zone/components/edit-zone/edit-zone.component.ts
+++ src/app/modules/branding/modules/zone/components/edit-zone/edit-zone.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-zone',
@@ -6,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-zone.component.scss']
 })
 export class EditZoneComponent implements OnInit {
+  public websiteId: number = null;
+  public zoneId: number = null;
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+    this.getParameters();
+  }
 
-  constructor() { }
+  private getParameters() {
+    const websiteId: number = parseInt(this.activatedRoute.snapshot.paramMap.get('websiteId'), 10);
+    const zoneId: number = parseInt(this.activatedRoute.snapshot.paramMap.get('zoneId'), 10);
+
+    if (websiteId && zoneId) {
+      this.zoneId = zoneId;
+      this.websiteId = websiteId;
+    } else {
+      this.router.navigateByUrl('/branding/zone/not-found');
+    }
+  }
 
   ngOnInit() {
   }
