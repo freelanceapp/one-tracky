79a571a29767bf8ba91e8968a0bc0a2a3a3138c6
aakash819 <aakash.goel462@gmail.com>
2019-08-26 14:21:22 +0530

    add code for get zone and website id

--- src/app/modules/branding/modules/zone/components/zone-properties/zone-properties.component.ts
+++ src/app/modules/branding/modules/zone/components/zone-properties/zone-properties.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-zone-properties',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ZonePropertiesComponent implements OnInit {
 
-  constructor() { }
+
+  public websiteId: number = null;
+  public zoneId: number = null;
+
+
+  constructor(private activatedRoute: ActivatedRoute) {
+    if (this.activatedRoute.parent.snapshot.paramMap.get('websiteId')) {
+      this.websiteId = parseInt(this.activatedRoute.parent.snapshot.paramMap.get('websiteId'), 10);
+    }
+    if (this.activatedRoute.parent.snapshot.paramMap.get('zoneId')) {
+      this.zoneId = parseInt(this.activatedRoute.parent.snapshot.paramMap.get('zoneId'), 10);
+    }
+
+  }
 
   ngOnInit() {
   }
