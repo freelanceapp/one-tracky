a0bc39999d0081e224411c7ec0b8c83ae1512278
aakash819 <aakash.goel462@gmail.com>
2019-08-29 18:17:52 +0530

    add new function for get dashboard

--- src/app/modules/branding/modules/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/modules/branding/modules/dashboard/components/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from 'src/app/modules/branding/services/dashboard/dashboard.service';
-import { BannerService } from 'src/app/modules/branding/services/banner/banner.service';
+import { HomeModel } from 'src/app/modules/branding/model/home.model';
+import { HttpService } from 'src/app/modules/branding/services/http/http.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,10 +9,21 @@ import { BannerService } from 'src/app/modules/branding/services/banner/banner.s
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  public dashBoard: HomeModel = null;
+  public errMsg: string = '';
 
-  constructor(private dashboard: BannerService) { }
+  constructor(private dashboardService: DashboardService) {
+  }
 
+  public getdashboard() {
+    this.dashboardService.getDashBoard()
+      .then(resp => { this.dashBoard = resp; })
+      .catch(err => {
+        this.errMsg = err;
+      });
+  }
   ngOnInit() {
+    this.getdashboard();
   }
 
 }
