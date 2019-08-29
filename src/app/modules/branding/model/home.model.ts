6b5e2950c8df42b18ebb06052e746905303fcba0
aakash819 <aakash.goel462@gmail.com>
2019-08-29 18:16:35 +0530

    add new model for dashboard

---
+++ src/app/modules/branding/model/home.model.ts
@@ -0,0 +1,7 @@
+export class HomeModel {
+    public requests: string = '';
+    public impressions: string = '';
+    public clicks: string = '';
+    public revenue: string = '';
+    public day: Date = null;
+}
