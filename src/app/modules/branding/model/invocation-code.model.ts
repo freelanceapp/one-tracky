5b98d5c4055777482b3f54cf988323298df1809c
aakash819 <aakash.goel462@gmail.com>
2019-09-02 15:53:12 +0530

    add new model for invocation code

---
+++ src/app/modules/branding/model/invocation-code.model.ts
@@ -0,0 +1,9 @@
+import { BannerTypeEnum } from '../enums/banner-type.enum';
+
+export class InvocationCodeModel {
+    public invocationCode: string = '';
+    public zoneId: number = null;
+    public websiteId: string = null;
+    public zoneType: string = '';
+    public thirdPartyTrack: string = '';
+}
