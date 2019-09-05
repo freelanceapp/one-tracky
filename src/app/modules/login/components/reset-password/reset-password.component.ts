9717082e4ae5743b7bf659f14b6296580e7c3679
aakash819 <aakash.goel462@gmail.com>
2019-09-05 18:01:27 +0530

    add code for reset password  on submit button

--- src/app/modules/login/components/reset-password/reset-password.component.ts
+++ src/app/modules/login/components/reset-password/reset-password.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ResetPasswordService } from 'src/app/services/reset-password/reset-password.service';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-reset-password',
@@ -10,8 +12,9 @@ export class ResetPasswordComponent implements OnInit {
 
   /** Reset password form */
   public rspForm: FormGroup;
+  public errMsg: string = ''
 
-  constructor() { }
+  constructor(private resetPasswordSrv: ResetPasswordService, private snackBar: MatSnackBar) { }
 
   ngOnInit() {
     this.createResetPasswordForm();
@@ -20,13 +23,30 @@ export class ResetPasswordComponent implements OnInit {
 
   private createResetPasswordForm() {
     this.rspForm = new FormGroup({
+      logInAs: new FormControl('3'),
       email: new FormControl('', [Validators.required, Validators.email])
     });
   }
 
   public onSubmit() {
     if (this.rspForm.valid) {
-      console.log(this.rspForm.value);
+      const email = this.rspForm.controls['email'].value;
+      const role = this.rspForm.controls['logInAs'].value;
+      this.resetPasswordSrv.resetPassword(email, role)
+        .then(res => {
+          if (res) {
+            this.snackBar.open('Password Sent successfully to your email address ', 'done', {
+              duration: 3000
+            });
+          } else {
+            this.snackBar.open('Email did not match!', 'done', {
+              duration: 3000
+            });
+          }
+        })
+        .catch(err => {
+          this.errMsg = err;
+        });
     }
   }
 
