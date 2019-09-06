51f253ee43ecbd038dcd97421dbf6592d653902d
aakash819 <aakash.goel462@gmail.com>
2019-09-06 15:31:25 +0530

    modify delete user function  add new function for get select id  add selection function

--- src/app/modules/branding/modules/users/components/all-users/all-users.component.ts
+++ src/app/modules/branding/modules/users/components/all-users/all-users.component.ts
@@ -4,6 +4,7 @@ import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 import { UserModel } from 'src/app/modules/branding/model/user.model';
 import { UserRole } from 'src/app/modules/branding/enums/user-role.enum';
 import { LoaderService } from 'src/app/modules/branding/services/loader/loader.service';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-all-users',
@@ -11,9 +12,10 @@ import { LoaderService } from 'src/app/modules/branding/services/loader/loader.s
   styleUrls: ['./all-users.component.scss']
 })
 export class AllUsersComponent implements OnInit {
+  selection = new SelectionModel<UserModel>(true, []);
 
 
-  displayedColumns: string[] = ['userId', 'fullName', 'email', 'createdDate', 'role', 'action'];
+  displayedColumns: string[] = ['select', 'userId', 'fullName', 'email', 'createdDate', 'role'];
   dataSource: MatTableDataSource<UserModel>;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -29,7 +31,26 @@ export class AllUsersComponent implements OnInit {
   ngOnInit() {
     this.getAllUsers();
   }
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
 
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: UserModel): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.userId}`;
+  }
   private getAllUsers() {
     this.loaderSvc.showloader = true;
     this.userSvc.getUsers()
@@ -49,6 +70,8 @@ export class AllUsersComponent implements OnInit {
     }
   }
 
+
+
   private updateUserTable() {
     this.dataSource = new MatTableDataSource(this.usersList);
     this.dataSource.paginator = this.paginator;
@@ -57,21 +80,27 @@ export class AllUsersComponent implements OnInit {
     }, 1000);
   }
 
-  public deleteUser(userId: number) {
+  public deleteUser() {
 
     if (!confirm('Are you sure to delete this user ?')) {
       return;
     }
     this.loaderSvc.showloader = true;
-    this.userSvc.deleteUser(userId).then(msg => {
-      alert(msg);
-      const index = this.usersList.findIndex(usr => usr.userId === userId);
-      this.usersList.splice(index, 1);
-      this.updateUserTable();
-    }).catch(err => alert(err))
-      .finally(() => this.loaderSvc.showloader = false);
-  }
+    this.userSvc.deleteUser(this.getCheckBoxId())
+      .then(msg => {
+        alert(msg);
+        // const index = this.usersList.findIndex(usr => usr.userId === userId);
+        // this.usersList.splice(index, 1);
+        this.updateUserTable();
+      })
+      .catch(err => alert(err))
+      .finally(() => { this.loaderSvc.showloader = false; this.selection.clear(); });
 
+  }
+  private getCheckBoxId(): number[] {
+    const userId = this.selection.selected.map(user => user.userId);
+    return userId;
+  }
 
   public getRloeName(roleId: number): string {
     return UserRole[roleId];
