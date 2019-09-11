df9fb2081536d5341c8dd52b73f1d380c5a7856b
aakash819 <aakash.goel462@gmail.com>
2019-09-11 14:00:44 +0530

    add functions for create form

--- src/app/modules/branding/modules/advertiser/components/user-access/user-access.component.ts
+++ src/app/modules/branding/modules/advertiser/components/user-access/user-access.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-access',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAccessComponent implements OnInit {
 
-  constructor() { }
+  public avdUserForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
+  public createAvdUserForm() {
+    return this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      userName: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      role: ['', Validators.required],
+      phoneNo: ['', Validators.required]
+    });
+  }
+
+
+  public onSubmit() {
+    console.log(this.avdUserForm.value)
+  }
   ngOnInit() {
+    this.avdUserForm = this.createAvdUserForm();
   }
 
 }
