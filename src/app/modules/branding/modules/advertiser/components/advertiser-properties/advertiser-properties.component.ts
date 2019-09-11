63edf9bf55e35475b8807827eeb0849a828a93c9
aakash819 <aakash.goel462@gmail.com>
2019-09-11 12:23:20 +0530

    add function for edit advertiser

--- src/app/modules/branding/modules/advertiser/components/advertiser-properties/advertiser-properties.component.ts
+++ src/app/modules/branding/modules/advertiser/components/advertiser-properties/advertiser-properties.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, AbstractControl, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { AdvertiserService } from 'src/app/modules/branding/services/advertiser/advertiser.service';
+import { MatSnackBar } from '@angular/material';
+import { AdvertiserModel } from 'src/app/modules/branding/model/advertiser.model';
+import { CustomValidators } from 'src/app/modules/branding/custom-validators/custom-validators';
 
 @Component({
   selector: 'app-advertiser-properties',
@@ -7,9 +13,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdvertiserPropertiesComponent implements OnInit {
 
-  constructor() { }
+  public isNewAdvertiser: boolean = true;
+  public advSrForm: FormGroup;
+  public advertiserId: number = null;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private fb: FormBuilder,
+    private advSvc: AdvertiserService,
+    private snackBar: MatSnackBar
+  ) {
+
+  }
 
   ngOnInit() {
+    this.createAdvSrForm().then(() => {
+      const advId: string = this.activatedRoute.parent.snapshot.paramMap.get('advertiserId');
+      this.advertiserId = parseInt(advId, 10);
+      if (this.advertiserId) {
+        this.getAndPatchAdvertiser();
+      }
+    });
   }
+
+
+  private createAdvSrForm(): Promise<boolean> {
+    this.advSrForm = this.fb.group({
+      advertiserId: [''],
+      advertiserName: ['', [Validators.required, CustomValidators.isAlphaNumericWithSpace]],
+      phoneNo: ['', Validators.required],
+      email: ['', [Validators.required, CustomValidators.isEmail]],
+      reportInterval: ['', [Validators.min(1)]],
+      comments: [''],
+      reportDeactivate: [false],
+      report: [false],
+    });
+    return Promise.resolve(true);
+  }
+
+
+  private patchAdvertiser(adv: AdvertiserModel) {
+    this.advSrForm.patchValue({
+      advertiserId: adv.advertiserId,
+      advertiserName: adv.advertiserName,
+      phoneNo: adv.phoneNo,
+      email: adv.email,
+      reportInterval: adv.reportInterval,
+      comments: adv.comments,
+      reportDeactivate: adv.reportDeactivate,
+      report: adv.report,
+    });
+  }
+
+  onSubmit() {
+    if (this.advSrForm.valid) {
+      this.advSvc.updateAdvertisers(this.advSrForm.value).then(msg => {
+        this.snackBar.open(msg, 'Ok');
+      }).catch(err => console.log(err));
+    }
+  }
+
+  private resetForm() {
+    this.advSrForm.reset();
+
+  }
+
+  private getAndPatchAdvertiser() {
+    this.advSvc.getAdvertisers(this.advertiserId).then(adv => {
+      this.patchAdvertiser(adv);
+    }).catch(err => {
+      alert(err);
+    });
+  }
+
 
 }
