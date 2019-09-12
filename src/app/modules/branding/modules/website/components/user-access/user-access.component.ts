4011ebfff210d6f30801b2dd1b43ef7e26a99856
aakash819 <aakash.goel462@gmail.com>
2019-09-12 11:34:55 +0530

    add function for add publiser user and get all user for user list

--- src/app/modules/branding/modules/website/components/user-access/user-access.component.ts
+++ src/app/modules/branding/modules/website/components/user-access/user-access.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { UserModel } from 'src/app/modules/branding/model/user.model';
 import { MatTableDataSource } from '@angular/material';
+import { WebsiteService } from 'src/app/modules/branding/services/website/website.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-access',
@@ -13,7 +15,11 @@ export class UserAccessComponent implements OnInit {
   dataSource: MatTableDataSource<UserModel>;
   public pubUserForm: FormGroup;
   public isAddUser: boolean = true;
-  constructor(private fb: FormBuilder) { }
+  public websiteId: number = null;
+  constructor(private fb: FormBuilder, private websiteSvc: WebsiteService, private activatedRoute: ActivatedRoute) {
+    this.websiteId = parseInt(this.activatedRoute.parent.snapshot.paramMap.get('websiteId'), 10);
+
+  }
 
   public createPubUserForm() {
     return this.fb.group({
@@ -22,16 +28,45 @@ export class UserAccessComponent implements OnInit {
       userName: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
       role: ['', Validators.required],
-      phoneNo: ['', Validators.required]
+      phoneNo: ['', Validators.required],
+      userType: 3
     });
   }
 
+  // private 
+
+
+
+  public getAllUsers() {
+    if (this.isAddUser) {
+      this.websiteSvc.getAllUsers(this.websiteId)
+        .then(resp => {
+          this.dataSource = new MatTableDataSource<UserModel>(resp)
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+
+
 
   public onSubmit() {
-    console.log(this.pubUserForm.value)
+    if (this.pubUserForm.valid) {
+      this.websiteSvc.addPublisherUser(this.websiteId, this.pubUserForm.value)
+        .then(resp => {
+          console.log(resp);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   }
+
+
   ngOnInit() {
     this.pubUserForm = this.createPubUserForm();
+    this.getAllUsers()
   }
 
 }
