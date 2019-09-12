42ca7a7400864940bd44cb3b5fd7b6ec505ac1be
aakash819 <aakash.goel462@gmail.com>
2019-09-12 18:52:03 +0530

    add function for get alluser ,adduser,edituser, getuserbyid set form data in case of edit

--- src/app/modules/branding/modules/advertiser/components/user-access/user-access.component.ts
+++ src/app/modules/branding/modules/advertiser/components/user-access/user-access.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { UserModel } from 'src/app/modules/branding/model/user.model';
-import { MatTableDataSource } from '@angular/material';
+import { MatTableDataSource, MatSnackBar } from '@angular/material';
+import { AdvertiserService } from 'src/app/modules/branding/services/advertiser/advertiser.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-access',
@@ -9,11 +11,31 @@ import { MatTableDataSource } from '@angular/material';
   styleUrls: ['./user-access.component.scss']
 })
 export class UserAccessComponent implements OnInit {
+
+  public avdId: number = null;
+  public userId: number = null;
+
   displayedColumns: string[] = ['email', 'fName', 'lName', 'date', 'action'];
   dataSource: MatTableDataSource<UserModel>;
   public avdUserForm: FormGroup;
   public isAddUser: boolean = true;
-  constructor(private fb: FormBuilder) { }
+  public isEdit: boolean = false;
+
+
+
+  constructor(private fb: FormBuilder, private advertiserSvc: AdvertiserService,
+    private activatedRoute: ActivatedRoute, private snackBar: MatSnackBar, private router: Router) {
+    this.avdId = parseInt(this.activatedRoute.parent.snapshot.paramMap.get('advertiserId'), 10);
+    this.userId = parseInt(this.activatedRoute.snapshot.paramMap.get('userId'), 10);
+    if (this.userId) {
+      this.getUserById();
+      this.isAddUser = false;
+      this.isEdit = true;
+    } else {
+      this.getAllUsers();
+    }
+
+  }
 
   public createAvdUserForm() {
     return this.fb.group({
@@ -22,13 +44,84 @@ export class UserAccessComponent implements OnInit {
       userName: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
       role: ['', Validators.required],
-      phoneNo: ['', Validators.required]
+      phone: ['', Validators.required],
+      userType: 2
+
+    });
+  }
+
+  private getUserList() {
+    this.isAddUser = !this.isAddUser;
+    this.getAllUsers();
+
+  }
+  public getAllUsers() {
+    if (this.isAddUser) {
+      this.advertiserSvc.getAllUsers(this.avdId)
+        .then(resp => {
+          this.dataSource = new MatTableDataSource<UserModel>(resp);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+
+
+
+  private addUser() {
+    this.router.navigateByUrl('/branding/advertiser/edit-advertiser/' + this.avdId + '/user-access');
+  }
+
+  public getUserById() {
+    this.advertiserSvc.getUserBYId(this.userId)
+      .then(resp => {
+        this.setUserForm(resp);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  private setUserForm(user: UserModel) {
+    this.avdUserForm.patchValue({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      userName: user.userName,
+      password: user.password,
+      role: user.role.toString(),
+      phone: user.phone,
+      userType: 2
     });
   }
 
 
+
+
   public onSubmit() {
-    console.log(this.avdUserForm.value)
+    if (this.avdUserForm.valid) {
+      if (this.userId) {
+        this.advertiserSvc.editUserBYId(this.userId, this.avdUserForm.value)
+          .then(resp => {
+            this.snackBar.open(resp, 'done', {
+              duration: 4000
+            });
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      } else {
+        this.advertiserSvc.addAdvertiserUser(this.avdId, this.avdUserForm.value)
+          .then(resp => {
+            this.snackBar.open(resp, 'done', {
+              duration: 4000
+            });
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
+    }
   }
   ngOnInit() {
     this.avdUserForm = this.createAvdUserForm();
